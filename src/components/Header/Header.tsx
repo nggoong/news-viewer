c3f81a16b1bc9719dc7a334de7573ef61c2f53e6
nggoong <nggoong@naver.com>
2022-10-18 09:50:14 +0900

    FEAT: 헤더 레이아웃 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,12 @@ const Header:React.FC = () => {
     return(
         <HeaderWrapper>
             <HeaderContent>
-
+                <HeaderLogo>
+                    <h1>news</h1>
+                </HeaderLogo>
+                <HeaderItems>
+                    dfsd
+                </HeaderItems>
             </HeaderContent>
         </HeaderWrapper>
     )
@@ -18,12 +23,29 @@ export default Header;
 const HeaderWrapper = styled.div`
     width:100%;
     height:60px;
-    background:green;
+    /* background:green; */
 `
 
 const HeaderContent = styled.div`
     width:80%;
     height:100%;
-    background:yellow;
+    /* background:yellow; */
     margin:0 auto;
+    display:flex;
+    justify-content:space-between;
+`
+
+const HeaderLogo = styled.div`
+    display:flex;
+    height:100%;
+    align-items:center;
+    h1 {
+        font-size:34px;
+    }
+`
+
+const HeaderItems = styled.div`
+    height:100%;
+    display:flex;
+    align-items:center;
 `
