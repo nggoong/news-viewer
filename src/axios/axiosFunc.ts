9dca25593516b209118d1cb693a7bba6a202032e
nggoong <nggoong@naver.com>
2022-10-21 12:57:03 +0900

    FIX: API 요청 시 URL에 undefined가 들어가 CORS 에러 발생하는 상황 수정 및 해결
    .env를 사용하여 API의 baseURL을 환경변수로 설정하여 사용하고 있었으나 url에 undefined가 포함되어 CORS에러가 발생하여 환경변수에서 제거하고 사용

--- src/axios/axiosFunc.ts
+++ src/axios/axiosFunc.ts
@@ -3,5 +3,5 @@ import { AxiosResponse } from 'axios';
 import { News } from '../model/news.model';
 
 export const fetchNewsBySearch:(searchInput:string, page:number)=>Promise<AxiosResponse<News>> = (searchInput, page)=> {
-    return instance.get(`${process.env.REACT_APP_BASE_URL}?q=${searchInput}&apikey=${process.env.REACT_APP_API_KEY}&page=${page}&sortBy=publishedAt`);
+    return instance.get(`https://newsapi.org/v2/everything?q=${searchInput}&apikey=${process.env.REACT_APP_API_KEY}&page=${page}&sortBy=publishedAt`);
 }
