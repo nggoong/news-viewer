4046c9dc1dcffac4e8c4a43279cfaef137ce0c5a
nggoong <nggoong@naver.com>
2022-10-21 14:04:08 +0900

    FEAT: 검색이 진행되는 동안 loading state를 toggle로 변경하는 로직 구현

--- src/redux/modules/newsSlice.ts
+++ src/redux/modules/newsSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { NewsArticlesType, News } from '../../model/news.model';
 import { FetchNewsType } from '../../model/fetchNewsPayload.model';
 import { fetchNewsBySearch } from '../../axios/axiosFunc';
+import { loadingActions } from './loadingSlice';
 
 interface InitialState{
     news:NewsArticlesType[] | any[];
@@ -14,15 +15,21 @@ const initialState:InitialState = {
 export const fetchBySearch = createAsyncThunk('news/fetchBySearch', async(payload:FetchNewsType, { dispatch }) => {
     let data:News;
     const { input, page } = payload;
+    const { setLoadingToggle } = loadingActions;
     try {
+        dispatch(setLoadingToggle());
         const res = await fetchNewsBySearch(input, page);
         data = res.data;
+        
     }
     catch(err) {
         console.log(err);
+        alert("에러가 발생했습니다. 잠시 후 실행해주세요!");
+        dispatch(setLoadingToggle());
         return;
     }
-
+    
+    dispatch(setLoadingToggle());
     return data;
     
 })
