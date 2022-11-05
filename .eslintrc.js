40df28ecb3b9033aa7cd0f970b6092c04b379bc9
nggoong <nggoong@naver.com>
2022-11-05 19:12:53 +0900

    FIX: eslint rules 추가
    prefer-const error >> warn, no-var error, eqeqeq error 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'prefer-const': 'warn', // let 사용 경고
+		'no-var': 'error', // var 금지
+		'eqeqeq': 'error', // == 사용 금지
+	},
+};
