c68e969bbde70a6aa6dc0558dcdd0cabcd438ce1
nggoong <nggoong@naver.com>
2022-12-07 16:49:03 +0900

    FEAT: 웹 폰트 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,6 +4,13 @@ const GlobalStyle = createGlobalStyle`
     * {
         margin:0;
         padding:0;
+        font-family: Cafe24ClassicType-Regular;
+    }
+    @font-face {
+        font-family: 'Cafe24ClassicType-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2210-2@1.0/Cafe24ClassicType-Regular.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
     }
 `;
 
