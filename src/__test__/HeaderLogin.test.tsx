05987dbde92b8bf0beb0317f2aa1f761b60b9a43
nggoong <nggoong@naver.com>
2022-12-10 20:53:46 +0900

    TEST: 헤더의 로그인 버튼을 누르면 자동으로 로그인이 되는 현상 테스트하는 코드 작성
    로그아웃 버튼이 있는지 없는지 검사하는 코드로 테스트

--- src/__test__/HeaderLogin.test.tsx
+++ src/__test__/HeaderLogin.test.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import store from '../redux/configStore';
 import App from '../App';
-import 'jest-styled-components';
 
-describe('테스트코드 테스트', () => {
-	test('렌더링 테스트', () => {
+describe('click login button that can open login modal', () => {
+	test('After opening modal, is there a login button?', () => {
 		render(
 			<MemoryRouter>
 				<Provider store={store}>
 					<App />
 				</Provider>
 			</MemoryRouter>,
-		).debug();
+		);
+
+		const loginButton = screen.getByText('로그인');
+		userEvent.click(loginButton);
+
+		screen.debug();
+
+		expect(!!screen.getAllByText('로그아웃')).toBe(false);
 	});
 });
