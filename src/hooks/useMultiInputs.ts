549d3a0cb3e954bef9c88cbf92050c05ec83c346
nggoong <nggoong@naver.com>
2022-12-20 12:13:35 +0900

    FIX: 커스텀 훅에서 사용된 useCallback()의 불필요한 의존성 제거
    useCallback의 의존성 배열에 state를 포함시켰으나, 불필요한 의존성이라 판단하여 제거

--- src/hooks/useMultiInputs.ts
+++ src/hooks/useMultiInputs.ts
@@ -10,13 +10,10 @@ const useMultiInputs = (
 	initialState: authInput,
 ): [authInput, React.Dispatch<React.SetStateAction<authInput>>, React.ChangeEventHandler<HTMLInputElement>] => {
 	const [state, setState] = useState(initialState);
-	const changeHandler = useCallback(
-		(e: React.ChangeEvent<HTMLInputElement>) => {
-			const { value, name } = e.target;
-			setState((prev) => ({ ...prev, [name]: value }));
-		},
-		[state],
-	);
+	const changeHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value, name } = e.target;
+		setState((prev) => ({ ...prev, [name]: value }));
+	}, []);
 
 	return [state, setState, changeHandler];
 };
--- src/hooks/useOnlyInput.ts
+++ src/hooks/useOnlyInput.ts
@@ -4,13 +4,10 @@ const useOnlyInput = (
 	initialState: string,
 ): [string, React.Dispatch<React.SetStateAction<string>>, React.ChangeEventHandler<HTMLInputElement>] => {
 	const [state, setState] = useState(initialState);
-	const changeHandler = useCallback(
-		(e: React.ChangeEvent<HTMLInputElement>) => {
-			const { value } = e.target;
-			setState(value);
-		},
-		[state],
-	);
+	const changeHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setState(value);
+	}, []);
 	return [state, setState, changeHandler];
 };
 export default useOnlyInput;
